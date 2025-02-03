26fa0852685056604564eec4c09e55e2647f1e35
Lolitron-0 <ovcharov_05@mail.ru>
2025-02-03 17:17:05 +0300

    tests improved

--- practice/practise-3/src/main/java/hse/kpo/services/CarService.java
+++ practice/practise-3/src/main/java/hse/kpo/services/CarService.java
@@ -6,6 +6,7 @@ import hse.kpo.interfaces.CarFactory;
 import hse.kpo.interfaces.CarProvider;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CarService implements CarProvider {
 
+    @Getter
     private final List<Car> cars = new ArrayList<>();
 
     private int carNumberCounter = 0;
