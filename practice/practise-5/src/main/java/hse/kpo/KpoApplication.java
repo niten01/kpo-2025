8362a57d522bf54b67a7750fadaa5e42837f7de6
Lolitron-0 <ovcharov_05@mail.ru>
2025-02-10 17:11:33 +0300

    report builders

--- practice/practise-5/src/main/java/hse/kpo/KpoApplication.java
+++ practice/practise-5/src/main/java/hse/kpo/KpoApplication.java
@@ -1,5 +1,8 @@
 package hse.kpo;
 
+import hse.kpo.reports.ReportBuilder;
+import hse.kpo.services.CustomerStorage;
+import hse.kpo.services.HseCarService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +12,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KpoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KpoApplication.class, args);
+        var context = SpringApplication.run(KpoApplication.class, args);
+        var hseCarService = context.getBean(HseCarService.class);
+        var customerStorage = context.getBean(CustomerStorage.class);
+
+        var reportBuilder = new ReportBuilder()
+                .addOperation("Инициализация системы")
+                .addCustomers(customerStorage.getCustomers());
+
+        hseCarService.sellCars();
+
+        var report = reportBuilder
+                .addOperation("Продажа автомобилей")
+                .addCustomers(customerStorage.getCustomers())
+                .build();
+
+        System.out.println(report.toString());
     }
 }
